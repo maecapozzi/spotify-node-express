cadf65629833e1c8f9acb7e46deb238fa532f594
Mae Capozzi <maecapozzi@gmail.com>
2017-09-13 16:34:10 -0400

    Replace previous authentication flow with passport strategy

--- app.js
+++ app.js
@@ -8,9 +8,33 @@ const cookieParser = require('cookie-parser')
 const clientId = process.env.SPOTIFY_CLIENT_ID
 const clientSecret = process.env.SPOTIFY_SECRET
 const cors = require('cors')
+const SpotifyStrategy = require('./lib/passport-spotify/index').Strategy
+const passport = require('passport')
 
 let LocalStorage = require('node-localstorage').LocalStorage
 localStorage = new LocalStorage('./localStorage')
+
+passport.serializeUser(function (user, done) {
+  done(null, user)
+})
+
+passport.deserializeUser(function (obj, done) {
+  done(null, obj)
+})
+
+passport.use(new SpotifyStrategy({
+  clientID: clientId,
+  clientSecret: clientSecret,
+  callbackURL: 'http://localhost:3001/callback'
+},
+  (accessToken, refreshToken, profile, done) => {
+    console.log(accessToken)
+    console.log(refreshToken)
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      return done(null, profile)
+    })
+  }))
 
 const app = express()
 
@@ -19,109 +43,26 @@ app.set('views', __dirname + '/app/views')
 app.set('view engine', 'pug')
 app.use(express.static(__dirname + '/public'))
   .use(cookieParser())
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.get('/callback', cors(), function (req, res) {
-  const stateKey = 'spotify_auth_state'
-  const state = req.query.state || null
-  const storedState = req.cookies ? req.cookies[stateKey] : null
+app.get('/auth/spotify',
+  passport.authenticate('spotify', {
+    scope: ['user-read-email', 'user-read-private'], showDialog: true
+  }), (req, res) => {
 
-  if (state === null || state !== storedState) {
-    res.redirect('/#' +
-      querystring.stringify({
-        error: 'state_mismatch'
-      })
-    )
-  } else {
-    res.clearCookie(stateKey)
-    requestTokensFromSpotify(req, res)
-  }
-})
-
-app.get('/refreshToken', cors(), function (req, res) {
-  const refreshToken = localStorage.getItem('refresh_token')
-  console.log(refreshToken)
-  const authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    form: {
-      refresh_token: refreshToken,
-      grant_type: 'refresh_token'
-    },
-    headers: {
-      'Authorization': 'Basic ' + (new Buffer(clientId + ':' + clientSecret).toString('base64'))
-    },
-    json: true
-  }
-
-  request.post(authOptions, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      const accessToken = body.access_token
-      const refreshToken = body.refresh_token
-
-      localStorage.setItem('access_token', accessToken)
-
-      res.redirect('/#' +
-        querystring.stringify({
-          access_token: accessToken,
-          refresh_token: refreshToken
-        })
-      )
-    } else {
-      res.redirect('/#' +
-        querystring.stringify({
-          error: 'invalid_token'
-        })
-      )
-    }
   })
-})
 
-const setTokens = (body, res) => {
-  const accessToken = body.access_token
-  const refreshToken = body.refresh_token
-  const expiresIn = body.expires_in
-
-  localStorage.setItem('access_token', accessToken)
-  localStorage.setItem('expires_in', expiresIn)
-  localStorage.setItem('refresh_token', refreshToken)
-
-  res.redirect('/#' +
-    querystring.stringify({
-      access_token: accessToken,
-      refresh_token: refreshToken
-    })
-  )
-}
-
-const requestTokensFromSpotify = (req, res) => {
-  const code = req.query.code || null
-  const redirectUri = 'http://localhost:3001/callback'
-
-  const authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    form: {
-      code: code,
-      redirect_uri: redirectUri,
-      grant_type: 'authorization_code'
-    },
-    headers: {
-      'Authorization': 'Basic ' + (new Buffer(clientId + ':' + clientSecret).toString('base64'))
-    },
-    json: true
-  }
-
-  request.post(authOptions, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      setTokens(body, res)
-    } else {
-      console.log('failure')
-      res.redirect('/#' +
-        querystring.stringify({
-          error: 'invalid_token'
-        })
-      )
-    }
+app.get('/callback',
+  passport.authenticate('spotify', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/')
   })
-}
+
+app.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/')
+})
 
 // access db auth from env
 const dbUsername = process.env.SPOTIFY_DB_USERNAME
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -10,36 +10,6 @@ routes.get('/', (req, res) => {
   res.render('index')
 })
 
-routes.get('/login', cors(), (req, res) => {
-  const clientId = process.env.SPOTIFY_CLIENT_ID
-  const redirectUri = 'http://localhost:3001/callback'
-  const stateKey = 'spotify_auth_state'
-  const state = authHelpers.generateRandomString(16)
-  const scope = 'user-read-private user-read-email'
-
-  res.cookie(stateKey, state)
-
-  // res.send('https://accounts.spotify.com/authorize?' +
-  //   querystring.stringify({
-  //     response_type: 'code',
-  //     client_id: clientId,
-  //     scope: scope,
-  //     redirect_uri: redirectUri,
-  //     state: state
-  //   })
-  // )
-
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-      response_type: 'code',
-      client_id: clientId,
-      scope: scope,
-      redirect_uri: redirectUri,
-      state: state
-    })
-  )
-})
-
 routes.get('/search', (req, res) => {
   const expiresIn = localStorage.getItem('expires_in')
   console.log(expiresIn)
