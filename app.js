9dd8a72bc78b4774b05689f1bb875e603d55a786
Mae Capozzi <maecapozzi@gmail.com>
2017-09-16 13:27:37 -0400

    Move all routes into app.js and make sure that i can capture access token

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const passport = require('passport')
 let LocalStorage = require('node-localstorage').LocalStorage
 localStorage = new LocalStorage('./localStorage')
 const SpotifyStrategy = require('./lib/passport-spotify/index').Strategy
+const cors = require('cors')
+const request = require('request')
 
 passport.serializeUser(function (user, done) {
   done(null, user)
@@ -21,11 +23,10 @@ passport.deserializeUser(function (obj, done) {
 passport.use(new SpotifyStrategy({
   clientID: clientId,
   clientSecret: clientSecret,
-  callbackURL: 'http://localhost:3000/callback'
+  callbackURL: 'http://localhost:3001/callback'
 },
   (accessToken, refreshToken, profile, done) => {
-    localStorage.removeItem('access_token')
-    localStorage.removeItem('refresh_token')
+    console.log(accessToken)
     localStorage.setItem('access_token', accessToken)
     localStorage.setItem('refresh_token', refreshToken)
 
@@ -43,6 +44,61 @@ app.use(express.static(__dirname + '/public'))
   .use(cookieParser())
 app.use(passport.initialize())
 app.use(passport.session())
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/auth/spotify',
+  passport.authenticate('spotify', {
+    scope: ['user-read-email', 'user-read-private'], showDialog: true
+  }), (req, res) => {})
+
+app.get('/callback',
+  passport.authenticate('spotify', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('http://localhost:3000')
+  })
+
+app.get('/refresh_token', function (req, res) {
+  var refresh_token = localStorage.getItem('refresh_token')
+
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(clientId + ':' + clientSecret).toString('base64'))
+    },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    json: true
+  }
+
+  request.post(authOptions, function (error, response, body) {
+    console.log(refresh_token)
+    console.log(response.body.error)
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token
+      res.send({
+        'access_token': access_token
+      })
+    }
+  })
+})
+
+app.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/')
+})
+app.get('/search', (req, res) => {
+  search.searchTracks(req, res)
+})
+
+app.get('/analyze/:id', cors(), (req, res) => {
+  const id = req.params.id
+  trackAnalysis.analyzeTrack(req, res, id)
+})
 
 const dbUsername = process.env.SPOTIFY_DB_USERNAME
 const dbPassword = process.env.SPOTIFY_DB_PASSWORD
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,65 +2,9 @@ const clientId = process.env.SPOTIFY_CLIENT_ID
 const clientSecret = process.env.SPOTIFY_SECRET
 const cors = require('cors')
 const passport = require('passport')
-const request = require('request')
+
 const routes = require('express').Router()
 const search = require('../helpers/search')
 const trackAnalysis = require('./tracks/audioAnalysis')
-
-routes.get('/', (req, res) => {
-  res.render('index')
-})
-
-routes.get('/auth/spotify',
-  passport.authenticate('spotify', {
-    scope: ['user-read-email', 'user-read-private'], showDialog: true
-  }), (req, res) => {})
-
-routes.get('/callback',
-  passport.authenticate('spotify', { failureRedirect: '/' }),
-  (req, res) => {
-    res.redirect('/')
-  })
-
-routes.get('/refresh_token', function (req, res) {
-  var refresh_token = localStorage.getItem('refresh_token')
-
-  var authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: {
-      'Authorization': 'Basic ' + (new Buffer(clientId + ':' + clientSecret).toString('base64'))
-    },
-    form: {
-      grant_type: 'refresh_token',
-      refresh_token: refresh_token
-    },
-    json: true
-  }
-
-  request.post(authOptions, function (error, response, body) {
-    console.log(refresh_token)
-    console.log(response.body.error)
-    if (!error && response.statusCode === 200) {
-      var access_token = body.access_token
-      res.send({
-        'access_token': access_token
-      })
-    }
-  })
-})
-
-routes.get('/logout', (req, res) => {
-  req.logout()
-  res.redirect('/')
-})
-
-routes.get('/search', (req, res) => {
-  search.searchTracks(req, res)
-})
-
-routes.get('/analyze/:id', cors(), (req, res) => {
-  const id = req.params.id
-  trackAnalysis.analyzeTrack(req, res, id)
-})
 
 module.exports = routes
