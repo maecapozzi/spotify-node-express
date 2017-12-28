d4fc4741893756f839400219e82352e9e8494dd9
maecapozzi <maecapozzi@gmail.com>
2017-12-28 17:07:04 -0500

    Update knexfile.js for db credentials

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 module.exports = {
   client: 'mysql',
   development: {
-    client: 'mysql',
     connection: {
       user: process.env.DEV_DB_USER,
       password: process.env.DEV_DB_PASSWORD,
