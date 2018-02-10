0ea3a1678d8654f4722239aa0e00ea69b2b60847
maecapozzi <maecapozzi@gmail.com>
2018-02-10 08:22:36 -0500

    Refactor app helpers

--- app/helpers/search.js
+++ app/helpers/search.js
@@ -1,29 +1,35 @@
 const request = require('request')
 
+const buildRequests = (req, res) => {
+  const accessToken = localStorage.getItem('access_token_' + req.session.id)
+  const options = {
+    url: 'https://api.spotify.com/v1/search?q=' + req.query.track + '&type=track',
+    headers: { Authorization: 'Bearer ' + accessToken },
+    json: true
+  }
+  return options
+}
+
+const handleSuccess = (response, res) => {
+  if (response.body.tracks === undefined) {
+    res.status(500).send('The search query was empty')
+  } else {
+    const results = []
+    response.body.tracks.items.map(item => {
+      results.push(item)
+    })
+    res.send({ results: results })
+  }
+}
+
 module.exports = {
   searchTracks: (req, res) => {
-    const accessToken = localStorage.getItem('access_token_' + req.session.id)
-
-    const options = {
-      url: 'https://api.spotify.com/v1/search?q=' + req.query.track + '&type=track',
-      headers: { 'Authorization': 'Bearer ' + accessToken },
-      json: true
-    }
-
-    request.get(options, (error, response) => {
+    request.get(buildRequests(req), (error, response) => {
       if (error) {
         res.status(500).send('Something is not working as expected.')
       } else {
         try {
-          if (response.body.tracks === undefined) { 
-            res.status(500).send('The search query was empty')
-          } else {
-            const results = []
-            response.body.tracks.items.map((item) => {
-              results.push(item)
-            })
-            res.send({ results: results })
-          }
+          handleSuccess(response, res)
         } catch (err) {
           res.status(500).send('Your search query is empty. Please input a search query into the search bar.')
         }
